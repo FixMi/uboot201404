400474673f6dd64b8d8f840b711e190f2578b652
numbqq <hbjsxieqi@163.com>
2017-01-17 16:50:53 +0800

    Set TZPC non-secure. Make sure DMA can work well.

---
+++ u-boot-2014.04/arch/arm/include/asm/arch-s5pc1xx/tzpc.h
@@ -0,0 +1,13 @@
+/*add by Nick. */
+#ifndef __TZPC__
+#define __TZPC__
+
+#define TZPC_DECPROT0SET_OFFSET			0x804
+#define TZPC_DECPROT1SET_OFFSET         0x810
+#define TZPC_DECPROT2SET_OFFSET         0x81C
+#define TZPC_DECPROT3SET_OFFSET         0x828
+
+#endif
+
+
+
